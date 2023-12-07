e4228b4b7afc958483ce56a85f116b261984d456
Sajjad Hossain Talukder <63524824+Sajjad-Hossain-Talukder@users.noreply.github.com>
2023-12-07 18:03:06 +0600

    Add new module

---
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Xotech on 07/12/2023.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+
+import UIKit
+
+class SecondViewController : UIViewController {
+    
+    
+}
--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    @IBOutlet weak var heightSlider: UISlider!
+    
+    
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func sliderActionReadHeight(_ sender: UISlider) {
+        heightLabel.text = String(format: "%.2f", sender.value )+" m"
+    }
+    
+    @IBAction func sliderActionReadWeight(_ sender: UISlider) {
+        weightLabel.text = String(Int(sender.value))+" kg"
+    }
+    
+    @IBAction func calculateButton(_ sender: UIButton) {
+        print( heightSlider.value ,weightSlider.value )
+        
+        var bmi = weightSlider.value / ( heightSlider.value * heightSlider.value )
+        
+        print(bmi)
+        
+        
+    }
 }
 
